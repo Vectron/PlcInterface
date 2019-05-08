e043e24265e1417d74935a1e80ab5dfbaf2530cf
Vectron <thijs_bloebaum@hotmail.com>
2019-05-08 13:37:01 +0200

    Add extension method to wait for a specific value

---
+++ src/PlcInterface.Abstraction/Extensions/IReadWriteExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PlcInterface.Abstraction.Extensions
+{
+    public static class IReadWriteExtension
+    {
+        /// <summary>
+        /// Read a tag untill we get the expected value, or a timeout happens
+        /// </summary>
+        /// <param name="tag">tag name to monitor</param>
+        /// <param name="value">the value to wait for</param>
+        /// <param name="timeout">time befor timeout exception is thrown</param>
+        public static void WaitForValue<T>(this IReadWrite readWrite, string tag, T value, TimeSpan timeout)
+        {
+            var source = new System.Threading.CancellationTokenSource(timeout);
+            var token = source.Token;
+            while (!readWrite.Read<T>(tag).Equals(value))
+            {
+                if (token.IsCancellationRequested)
+                {
+                    throw new TimeoutException($"Couldnt get a proper response from the PLC in {timeout.TotalSeconds} seconds");
+                }
+            }
+        }
+    }
+}

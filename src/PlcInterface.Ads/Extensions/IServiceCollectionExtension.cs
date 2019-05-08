72f2bde0577bd54c5721471bbbb839ae037fae51
Vectron <thijs_bloebaum@hotmail.com>
2019-05-08 13:31:47 +0200

    add a method so we can add the plc items to DI

---
+++ src/PlcInterface.Ads/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TwinCAT.Ads;
+
+namespace PlcInterface.Ads.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddPLC(this IServiceCollection serviceDescriptors)
+            => serviceDescriptors
+                .AddSingleton(typeof(ReadWrite), typeof(ReadWrite))
+                .AddSingleton(typeof(IReadWrite), x => x.GetService<ReadWrite>())
+                .AddSingleton(typeof(Monitor), typeof(Monitor))
+                .AddSingleton(typeof(IMonitor), x => x.GetService<Monitor>())
+                .AddSingleton(typeof(SymbolHandler), typeof(SymbolHandler))
+                .AddSingleton(typeof(ISymbolHandler), x => x.GetService<SymbolHandler>())
+                .AddSingleton(typeof(PlcConnection), typeof(PlcConnection))
+                .AddSingleton(typeof(IPlcConnection<IAdsConnection>), x => x.GetService<PlcConnection>())
+                .AddSingleton(typeof(IPlcConnection), x => x.GetService<IPlcConnection<IAdsConnection>>())
+                .AddSingleton(typeof(DynamicValueConverter), typeof(DynamicValueConverter))
+                .AddSingleton(typeof(IDynamicValueConverter), x => x.GetService<DynamicValueConverter>());
+    }
+}
---
+++ src/PlcInterface.OpcUa/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PlcInterface.OpcUa.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddPLC(this IServiceCollection serviceDescriptors)
+            => serviceDescriptors
+                .AddSingleton(typeof(IReadWrite), x => x.GetService<ReadWrite>())
+                .AddSingleton(typeof(ReadWrite), typeof(ReadWrite))
+                .AddSingleton(typeof(IMonitor), x => x.GetService<Monitor>())
+                .AddSingleton(typeof(Monitor), typeof(Monitor))
+                .AddSingleton(typeof(ISymbolHandler), x => x.GetService<SymbolHandler>())
+                .AddSingleton(typeof(SymbolHandler), typeof(SymbolHandler))
+                .AddSingleton(typeof(PlcConnection), typeof(PlcConnection))
+                .AddSingleton(typeof(IPlcConnection<Opc.Ua.Client.Session>), x => x.GetService<PlcConnection>())
+                .AddSingleton(typeof(IPlcConnection), x => x.GetService<IPlcConnection<Opc.Ua.Client.Session>>());
+    }
+}

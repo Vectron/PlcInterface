d169a529dd336f84d993656c334ab22aca4a03d5
Vectron <thijs_bloebaum@hotmail.com>
2021-07-23 21:28:10 +0200

    [ADS] registering multiple IO at the same time should follow the normal register path. fixes unsubscription not working

--- src/PlcInterface.Ads/Monitor.cs
+++ src/PlcInterface.Ads/Monitor.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using Microsoft.Extensions.Logging;
-using TwinCAT.Ads;
-using TwinCAT.Ads.Reactive;
-using TwinCAT.TypeSystem;
 
 namespace PlcInterface.Ads
 {
@@ -15,7 +11,6 @@ namespace PlcInterface.Ads
     /// </summary>
     public class Monitor : IMonitor
     {
-        private readonly IPlcConnection<IAdsConnection> connection;
         private readonly ILogger logger;
         private readonly Dictionary<string, DisposableMonitorItem> streams = new(StringComparer.OrdinalIgnoreCase);
         private readonly ISymbolHandler symbolHandler;
@@ -25,13 +20,11 @@ namespace PlcInterface.Ads
         /// <summary>
         /// Initializes a new instance of the <see cref="Monitor"/> class.
         /// </summary>
-        /// <param name="connection">A <see cref="IPlcConnection{T}"/> implementation.</param>
         /// <param name="symbolHandler">A <see cref="ISymbolHandler"/> implementation.</param>
         /// <param name="typeConverter">A <see cref="ITypeConverter"/> implementation.</param>
         /// <param name="logger">A <see cref="ILogger"/> implementation.</param>
-        public Monitor(IPlcConnection<IAdsConnection> connection, ISymbolHandler symbolHandler, IAdsTypeConverter typeConverter, ILogger<Monitor> logger)
+        public Monitor(ISymbolHandler symbolHandler, IAdsTypeConverter typeConverter, ILogger<Monitor> logger)
         {
-            this.connection = connection;
             this.symbolHandler = symbolHandler;
             this.typeConverter = typeConverter;
             this.logger = logger;
@@ -48,30 +41,9 @@ namespace PlcInterface.Ads
         /// <inheritdoc/>
         public void RegisterIO(IEnumerable<string> ioNames, int updateInterval = 1000)
         {
-            var symbols = ioNames
-                .Where(x => !streams.ContainsKey(x))
-                .Select(x => symbolHandler.GetSymbolinfo(x))
-                .Cast<SymbolInfo>()
-                .Select(x => x.Symbol)
-                .Cast<IValueSymbol>();
-
-            var subscription = connection.GetConnectedClient()
-                    .WhenValueChangedAnnotated(symbols)
-                    .Select(x => new MonitorResult(x.Symbol.InstancePath, x.Value))
-                    .Subscribe(x => symbolStream.OnNext(x));
-
-            var monitoredItem = new DisposableMonitorItem(subscription);
-
             foreach (var ioName in ioNames)
             {
-                if (streams.TryGetValue(ioName, out var disposableMonitorItem))
-                {
-                    disposableMonitorItem.Subscriptions += 1;
-                    continue;
-                }
-
-                streams.Add(ioName, monitoredItem);
-                monitoredItem.Subscriptions += 1;
+                RegisterIO(ioName, updateInterval);
             }
         }
 

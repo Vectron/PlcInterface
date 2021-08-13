18c6031c11c056c1a2075d3dce5aeb7194ddcdf7
Vectron <thijs_bloebaum@hotmail.com>
2021-08-13 23:08:22 +0200

    [ADS.Tests] fix service collection tests

--- test/PlcInterface.Ads.Tests/IServiceCollectionExtensionTests.cs
+++ test/PlcInterface.Ads.Tests/IServiceCollectionExtensionTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace PlcInterface.Ads.Tests
 {
@@ -12,17 +13,21 @@ namespace PlcInterface.Ads.Tests
 
         [ClassCleanup]
         public static void CleanDI()
-            => provider?.Dispose();
+            => provider!.Dispose();
 
         [ClassInitialize]
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Public Api")]
         public static void SetupDI(TestContext testContext)
             => provider = new ServiceCollection()
-               .AddOptions()
-               .AddSingleton<ILoggerFactory, NullLoggerFactory>()
-               .AddSingleton(typeof(ILogger<>), typeof(NullLogger<>))
-               .AddAdsPLC()
-               .BuildServiceProvider(new ServiceProviderOptions() { ValidateOnBuild = true, ValidateScopes = true });
+            .AddOptions()
+            .AddSingleton<ILoggerFactory, NullLoggerFactory>()
+            .AddSingleton(typeof(ILogger<>), typeof(NullLogger<>))
+            .AddAdsPLC()
+            .AddSingleton(Mock.Of<IPlcConnection>())
+            .AddSingleton(Mock.Of<ISymbolHandler>())
+            .AddSingleton(Mock.Of<IMonitor>())
+            .AddSingleton(Mock.Of<IReadWrite>())
+            .BuildServiceProvider(new ServiceProviderOptions() { ValidateOnBuild = true, ValidateScopes = true });
 
         [TestMethod]
         public void MonitorIsregisterCorrect()
@@ -30,11 +35,10 @@ namespace PlcInterface.Ads.Tests
             Assert.IsNotNull(provider);
 
             var adsMonitor = provider.GetRequiredService<IAdsMonitor>();
-            var monitor = provider.GetRequiredService<IMonitor>();
             using var concrete = provider.GetService<Monitor>();
 
             Assert.IsNull(concrete);
-            Assert.AreSame(adsMonitor, monitor);
+            Assert.IsInstanceOfType(adsMonitor, typeof(Monitor));
         }
 
         [TestMethod]
@@ -43,12 +47,11 @@ namespace PlcInterface.Ads.Tests
             Assert.IsNotNull(provider);
 
             var adsConnection = provider.GetRequiredService<IAdsPlcConnection>();
-            var connection = provider.GetRequiredService<IPlcConnection>();
             var genericConnection = provider.GetRequiredService<IPlcConnection<TwinCAT.Ads.IAdsConnection>>();
             using var concrete = provider.GetService<PlcConnection>();
 
             Assert.IsNull(concrete);
-            Assert.AreSame(adsConnection, connection);
+            Assert.IsInstanceOfType(adsConnection, typeof(PlcConnection));
             Assert.AreSame(adsConnection, genericConnection);
         }
 
@@ -58,11 +61,10 @@ namespace PlcInterface.Ads.Tests
             Assert.IsNotNull(provider);
 
             var adsReadWrite = provider.GetRequiredService<IAdsReadWrite>();
-            var readWrite = provider.GetRequiredService<IReadWrite>();
             var concrete = provider.GetService<ReadWrite>();
 
             Assert.IsNull(concrete);
-            Assert.AreSame(adsReadWrite, readWrite);
+            Assert.IsInstanceOfType(adsReadWrite, typeof(ReadWrite));
         }
 
         [TestMethod]
@@ -71,11 +73,10 @@ namespace PlcInterface.Ads.Tests
             Assert.IsNotNull(provider);
 
             var adsSymbolHandler = provider.GetRequiredService<IAdsSymbolHandler>();
-            var symbolHandler = provider.GetRequiredService<ISymbolHandler>();
             using var concrete = provider.GetService<SymbolHandler>();
 
             Assert.IsNull(concrete);
-            Assert.AreSame(adsSymbolHandler, symbolHandler);
+            Assert.IsInstanceOfType(adsSymbolHandler, typeof(SymbolHandler));
         }
     }
 }

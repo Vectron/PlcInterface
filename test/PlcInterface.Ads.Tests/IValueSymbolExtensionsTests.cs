224b5409a28acca12e27373b6f9e92801d760d29
Vectron <thijs_bloebaum@hotmail.com>
2021-08-17 15:40:38 +0200

    [Tests.Ads] Add tests for IValueSymbolExtensions

---
+++ test/PlcInterface.Ads.Tests/IValueSymbolExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TwinCAT.TypeSystem;
+
+namespace PlcInterface.Ads.Tests
+{
+    [TestClass]
+    public class IValueSymbolExtensionsTests
+    {
+        [TestMethod]
+        public void CastAndValidateReturnsSymbolInfo()
+        {
+            // Arrange
+            var symbolMock = new Mock<ISymbol>();
+            _ = symbolMock.As<IValueSymbol>();
+
+            // Act
+            var actual = symbolMock.Object.CastAndValidate();
+
+            // Assert
+            Assert.IsInstanceOfType(actual, typeof(IValueSymbol));
+            Assert.AreSame(symbolMock.Object, actual);
+        }
+
+        [TestMethod]
+        public void CastAndValidateThrowsSymbolExceptionWhenNotAdsSymbol()
+        {
+            // Arrange
+            var symbolMock = Mock.Of<ISymbol>();
+
+            // Act
+            // Assert
+            _ = Assert.ThrowsException<SymbolException>(symbolMock.CastAndValidate);
+        }
+    }
+}

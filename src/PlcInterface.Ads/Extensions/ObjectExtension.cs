368f92a8a3a7df0dd0a211e43ac7a9bf6df20c37
Vectron <thijs_bloebaum@hotmail.com>
2022-07-15 11:41:09 +0200

    Move extension to root namespace

--- src/PlcInterface.Ads/Extensions/ObjectExtension.cs
+++ src/PlcInterface.Ads/Extensions/ObjectExtension.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace PlcInterface.Ads.Extensions;
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+
+namespace PlcInterface.Ads;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
 
 /// <summary>
 /// Extension methods for any object.

568df15d3149cb92adda67e66d0db597e77a184d
Vectron <thijs_bloebaum@hotmail.com>
2023-04-17 13:17:36 +0200

    [ADS, Abstraction] rewrite the type converter

--- src/PlcInterface.Ads/AdsTypeConverter.cs
+++ src/PlcInterface.Ads/AdsTypeConverter.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Dynamic;
 using System.Globalization;
-using TwinCAT.Ads.TypeSystem;
 using TwinCAT.TypeSystem;
 
 namespace PlcInterface.Ads;
@@ -37,182 +35,51 @@ public sealed class AdsTypeConverter : TypeConverter, IAdsTypeConverter
     /// <inheritdoc/>
     public override object Convert(object value, Type targetType)
     {
-        if (value is DynamicObject dynamicObject)
+        if (value is TwinCAT.PlcOpen.DateBase dateBase)
         {
-            return ConvertFrom(dynamicObject, targetType);
+            if (targetType == typeof(DateTimeOffset))
+            {
+                return new DateTimeOffset(dateBase.Value);
+            }
+
+            return dateBase.Value;
         }
 
-        if (value is DateTime dateTime && targetType == typeof(DateTimeOffset))
+        if (value is TwinCAT.PlcOpen.TimeBase timeBase)
         {
-            return new DateTimeOffset(dateTime);
+            return timeBase.Time;
         }
 
-        if (targetType.IsArray && value is Array dynamicObjects)
+        if (value is TwinCAT.PlcOpen.LTimeBase lTimeBase)
         {
-            return ConvertArray(dynamicObjects, targetType);
+            return lTimeBase.Time;
+        }
+
+        if (value is IDynamicValue valueObject && valueObject.DataType is IArrayType arrayType)
+        {
+            return ConvertDynamicValueArray(valueObject, arrayType, targetType);
         }
 
         return base.Convert(value, targetType);
     }
 
-    private Array ConvertArray(Array dynamicObjects, Type type)
+    private object ConvertDynamicValueArray(IDynamicValue valueObject, IArrayType arrayType, Type targetType)
     {
-        var elementType = type.GetElementType() ?? throw new NotSupportedException($"Unable to retrieve element type");
-        var dimensionLengts = new int[dynamicObjects.Rank];
-        for (var i = 0; i < dynamicObjects.Rank; i++)
-        {
-            dimensionLengts[i] = dynamicObjects.GetLength(i);
-        }
-
-        var destination = Array.CreateInstance(elementType, dimensionLengts);
-        foreach (var indices in IndicesHelper.GetIndices(destination))
-        {
-            object? result;
-            try
-            {
-                result = dynamicObjects.GetValue(indices);
-            }
-            catch (Exception)
-            {
-                throw new SymbolException($"No value found at index {indices}");
-            }
-
-            if (result == null)
-            {
-                var canBeNull = !elementType.IsValueType || (Nullable.GetUnderlyingType(elementType) != null);
-                if (!canBeNull)
-                {
-                    throw new SymbolException($"Can't assign null to array");
-                }
-
-                destination.SetValue(result, indices);
-            }
-            else if (result is DynamicObject resultDynamicObject)
-            {
-                var value = ConvertFrom(resultDynamicObject, elementType);
-                destination.SetValue(value, indices);
-            }
-            else if (result.GetType() == elementType)
-            {
-                destination.SetValue(result, indices);
-            }
-            else
-            {
-                throw new SymbolException($"Unable to convert type {result}");
-            }
-        }
-
-        return destination;
-    }
-
-    private Array ConvertArray(DynamicObject dynamicObject, Type type)
-    {
-        if (dynamicObject is not IDynamicValue valueObject)
-        {
-            throw new NotSupportedException($"dynamic object is not a {typeof(IDynamicValue)}");
-        }
-
-        if (valueObject.DataType is not IArrayType dataType)
-        {
-            throw new NotSupportedException($"Data Type is not a {typeof(ArrayType)}");
-        }
-
-        var ellementType = type.GetElementType()
+        var elementType = targetType.GetElementType()
             ?? throw new NotSupportedException($"Unable to retrieve element type");
-        var dimensionLengts = dataType.Dimensions.GetDimensionLengths();
-        var destination = Array.CreateInstance(ellementType, dimensionLengts);
+        var dimensionLengts = arrayType.Dimensions.GetDimensionLengths();
+        var destination = Array.CreateInstance(elementType, dimensionLengts);
 
         foreach (var indices in IndicesHelper.GetIndices(destination))
         {
-            if (!valueObject.TryGetIndexValue(indices, out var result))
+            if (!valueObject.TryGetIndexValue(indices, out var memberValue))
             {
                 throw new SymbolException($"No value found at index {indices}");
             }
 
-            if (result is DynamicObject resultDynamicObject)
-            {
-                var value = ConvertFrom(resultDynamicObject, ellementType);
-                destination.SetValue(value, indices);
-            }
-            else if (result.GetType() == ellementType)
-            {
-                destination.SetValue(result, indices);
-            }
-            else
-            {
-                throw new SymbolException($"Unable to convert type {result}");
-            }
+            destination.SetValue(Convert(memberValue, elementType), indices);
         }
 
         return destination;
-    }
-
-    private object ConvertFrom(DynamicObject dynamicObject, Type type)
-    {
-        if (type.IsArray)
-        {
-            return ConvertArray(dynamicObject, type);
-        }
-
-        var destination = Activator.CreateInstance(type) ?? throw new NotSupportedException($"Unable to create a instance for type: {type.Name}");
-        foreach (var memberName in dynamicObject.GetDynamicMemberNames())
-        {
-            var property = type.GetProperty(memberName)
-                ?? throw new InvalidOperationException($"{memberName} not found as a property");
-
-            if (!property.CanWrite)
-            {
-                throw new InvalidOperationException($"{property.Name} is not writable");
-            }
-
-            if (!dynamicObject.TryGetMember(new MemberBinder(property.Name, true), out var result))
-            {
-                throw new InvalidOperationException($"{property.Name} is not found in the PLC type");
-            }
-
-            if (result is DynamicObject resultDynamicObject)
-            {
-                var value = ConvertFrom(resultDynamicObject, property.PropertyType);
-                property.SetValue(destination, value);
-            }
-            else if (result is TwinCAT.PlcOpen.DateBase dateBase)
-            {
-                if (property.PropertyType == typeof(DateTimeOffset))
-                {
-                    property.SetValue(destination, new DateTimeOffset(dateBase.Value));
-                }
-                else
-                {
-                    property.SetValue(destination, dateBase.Value);
-                }
-            }
-            else if (result is TwinCAT.PlcOpen.TimeBase timeBase)
-            {
-                property.SetValue(destination, timeBase.Time);
-            }
-            else if (result is TwinCAT.PlcOpen.LTimeBase lTimeBase)
-            {
-                property.SetValue(destination, lTimeBase.Time);
-            }
-            else
-            {
-                property.SetValue(destination, result);
-                continue;
-            }
-        }
-
-        return destination;
-    }
-
-    private sealed class MemberBinder : GetMemberBinder
-    {
-        public MemberBinder(string name, bool ignoreCase)
-            : base(name, ignoreCase)
-        {
-        }
-
-        [ExcludeFromCodeCoverage]
-        public override DynamicMetaObject FallbackGetMember(DynamicMetaObject target, DynamicMetaObject? errorSuggestion)
-            => throw new NotSupportedException();
     }
 }

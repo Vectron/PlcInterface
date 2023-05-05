6137e6c20c54e332c1d49de29a9d985c83ee3bd6
Vectron <thijs_bloebaum@hotmail.com>
2023-05-05 13:34:50 +0200

    [Abstraction.Tests] add tests for the activator helpers

---
+++ test/PlcInterface.Abstraction/PropertySetterHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace PlcInterface.Abstraction.Tests;
+
+[TestClass]
+public class PropertySetterHelperTests
+{
+    [TestMethod]
+    public void WhenDeclaringTypeIsNullThrowsNotSupportedException()
+    {
+        // Arrange
+        var constructor = Mock.Of<PropertyInfo>(x => x.DeclaringType == null);
+
+        // Act Assert
+        _ = Assert.ThrowsException<NotSupportedException>(() => new PropertySetterHelper(constructor));
+    }
+}

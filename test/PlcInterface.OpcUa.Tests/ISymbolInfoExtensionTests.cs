83e7f35a2bb055ac9d1dd986c39fa50ac3c41094
Vectron <thijs_bloebaum@hotmail.com>
2023-06-21 12:37:08 +0200

    [OPC] Add test for the ConvertAndValidate extension

---
+++ test/PlcInterface.OpcUa.Tests/ISymbolInfoExtensionTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace PlcInterface.OpcUa.Tests;
+
+[TestClass]
+public class ISymbolInfoExtensionTests
+{
+    [TestMethod]
+    public void CastAndValidateReturnsSymbolInfo()
+    {
+        // Arrange
+        ISymbolInfo symbolInfo = Mock.Of<IOpcSymbolInfo>();
+
+        // Act
+        var actual = symbolInfo.ConvertAndValidate();
+
+        // Assert
+        Assert.IsInstanceOfType(actual, typeof(IOpcSymbolInfo));
+        Assert.IsInstanceOfType(actual, typeof(ISymbolInfo));
+        Assert.AreSame(symbolInfo, actual);
+    }
+
+    [TestMethod]
+    public void CastAndValidateThrowsSymbolExceptionWhenNotAdsSymbol()
+    {
+        // Arrange
+        var symbolMock = Mock.Of<ISymbolInfo>();
+
+        // Act Assert
+        _ = Assert.ThrowsException<SymbolException>(symbolMock.ConvertAndValidate);
+    }
+}

6eee454fafcf1c4de18732c3c82b7868689d3801
Vectron <thijs_bloebaum@hotmail.com>
2024-02-08 15:02:13 +0100

    Fix LogExceptionsAsyncLogsExceptionsToErrorStream not passing

--- test/PlcInterface.Common.Tests/TaskExtensionsTests.cs
+++ test/PlcInterface.Common.Tests/TaskExtensionsTests.cs
@@ -14,13 +14,23 @@ public class TaskExtensionsTests
     {
         // Arrange
         var loggerMock = new Mock<ILogger>();
+        _ = loggerMock
+            .Setup(x => x.IsEnabled(It.IsAny<LogLevel>()))
+            .Returns(value: true);
 
         // Act
         var task2 = Task.Run(() => throw new NotSupportedException()).LogExceptionsAsync(loggerMock.Object);
         task2.Wait();
 
         // Assert
-        loggerMock.Verify(x => x.Log(It.Is<LogLevel>(x => x == LogLevel.Error), It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(), It.IsAny<NotSupportedException>(), It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
+        loggerMock.Verify(
+            x => x.Log(
+                It.Is<LogLevel>(x => x == LogLevel.Error),
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<NotSupportedException>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
     }
 
     [TestMethod]

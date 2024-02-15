feb90d0d3338c3b79167eb7ead7b00dbf7044c6f
Vectron <thijs_bloebaum@hotmail.com>
2024-02-15 09:20:29 +0100

    [Ads.Test] fix mock creation

--- test/PlcInterface.Ads.Tests/ISymbolInfoExtensionTests.cs
+++ test/PlcInterface.Ads.Tests/ISymbolInfoExtensionTests.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TwinCAT.TypeSystem;
@@ -11,7 +12,7 @@ public class ISymbolInfoExtensionTests
     public void CastAndValidateReturnsSymbolInfo()
     {
         // Arrange
-        ISymbolInfo symbolInfo = new SymbolInfo(Mock.Of<ISymbol>());
+        ISymbolInfo symbolInfo = new SymbolInfo(Mock.Of<ISymbol>(), string.Empty);
 
         // Act
         var actual = symbolInfo.CastAndValidate();
@@ -31,5 +32,93 @@ public class ISymbolInfoExtensionTests
         // Act
         // Assert
         _ = Assert.ThrowsException<SymbolException>(symbolMock.CastAndValidate);
+    }
+
+    private sealed class DummySymbol : ISymbol
+    {
+        public ITypeAttributeCollection Attributes => null!;
+
+        public int BitSize => 0;
+
+        public int ByteSize => 0;
+
+        public DataTypeCategory Category
+        {
+            get;
+        }
+
+        public string Comment => string.Empty;
+
+        public IDataType? DataType
+        {
+            get;
+        }
+
+        public string InstanceName => "DummyVar1";
+
+        public string InstancePath => "DummyVar1";
+
+        public bool IsBitType => true;
+
+        public bool IsByteAligned
+        {
+            get;
+        }
+
+        public bool IsContainerType
+        {
+            get;
+        }
+
+        public bool IsPersistent
+        {
+            get;
+        }
+
+        public bool IsPointer
+        {
+            get;
+        }
+
+        public bool IsPrimitiveType
+        {
+            get;
+        }
+
+        public bool IsReadOnly
+        {
+            get;
+        }
+
+        public bool IsRecursive
+        {
+            get;
+        }
+
+        public bool IsReference
+        {
+            get;
+        }
+
+        public bool IsStatic
+        {
+            get;
+        }
+
+        public ISymbol? Parent
+        {
+            get;
+        }
+
+        public int Size
+        {
+            get;
+        }
+
+        public ISymbolCollection<ISymbol> SubSymbols => new SymbolCollection();
+
+        public string TypeName => "Boolean";
+
+        public Encoding ValueEncoding => null!;
     }
 }

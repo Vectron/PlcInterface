a859669926451a0158a078eee98415e0841568fb
Vectron <thijs_bloebaum@hotmail.com>
2024-02-19 10:04:22 +0100

    Remove unused class

--- test/PlcInterface.Ads.Tests/ISymbolInfoExtensionTests.cs
+++ test/PlcInterface.Ads.Tests/ISymbolInfoExtensionTests.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TwinCAT.TypeSystem;
@@ -32,93 +31,5 @@ public class ISymbolInfoExtensionTests
         // Act
         // Assert
         _ = Assert.ThrowsException<SymbolException>(symbolMock.CastAndValidate);
-    }
-
-    private sealed class DummySymbol : ISymbol
-    {
-        public ITypeAttributeCollection Attributes => null!;
-
-        public int BitSize => 0;
-
-        public int ByteSize => 0;
-
-        public DataTypeCategory Category
-        {
-            get;
-        }
-
-        public string Comment => string.Empty;
-
-        public IDataType? DataType
-        {
-            get;
-        }
-
-        public string InstanceName => "DummyVar1";
-
-        public string InstancePath => "DummyVar1";
-
-        public bool IsBitType => true;
-
-        public bool IsByteAligned
-        {
-            get;
-        }
-
-        public bool IsContainerType
-        {
-            get;
-        }
-
-        public bool IsPersistent
-        {
-            get;
-        }
-
-        public bool IsPointer
-        {
-            get;
-        }
-
-        public bool IsPrimitiveType
-        {
-            get;
-        }
-
-        public bool IsReadOnly
-        {
-            get;
-        }
-
-        public bool IsRecursive
-        {
-            get;
-        }
-
-        public bool IsReference
-        {
-            get;
-        }
-
-        public bool IsStatic
-        {
-            get;
-        }
-
-        public ISymbol? Parent
-        {
-            get;
-        }
-
-        public int Size
-        {
-            get;
-        }
-
-        public ISymbolCollection<ISymbol> SubSymbols => new SymbolCollection();
-
-        public string TypeName => "Boolean";
-
-        public Encoding ValueEncoding => null!;
     }
 }
